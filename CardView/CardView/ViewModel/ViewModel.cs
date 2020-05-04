f7f273dda18717fbce404d0db6cf1e51a3138e22
Anirudhan Iyyappan <anirudhan.iyyappan@syncfusion.com>
2020-05-04 16:35:54 +0530

    Sample added

---
+++ CardView/CardView/ViewModel/ViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardView
+{
+    public class ViewModel
+    {
+        public ObservableCollection<Contact> Contacts { get; set; }
+        public ViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>();
+            PopulateData();
+        }
+        private void PopulateData()
+        {
+            Contacts.Add(new Contact() { Name = "John", Age = 26 });
+            Contacts.Add(new Contact() { Name = "Mark", Age = 25 });
+            Contacts.Add(new Contact() { Name = "Steven", Age = 26 });
+        }
+    }
+}
